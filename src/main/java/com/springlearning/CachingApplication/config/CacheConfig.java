14552a79421459c13a5a6ff98b54e32f98986561
fardeen-autonomize <fardeen.khan@autonomize.ai>
2025-08-03 20:37:55 +0530

    W10 L3

--- src/main/java/com/springlearning/CachingApplication/config/CacheConfig.java
+++ src/main/java/com/springlearning/CachingApplication/config/CacheConfig.java
@@ -1,9 +1,32 @@
 package com.springlearning.CachingApplication.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson3JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+
+    @Bean
+    public CacheManager getRedisCacheManager(RedisConnectionFactory redisConnectionFactory){
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .prefixCacheNameWith("my-redis-")
+                .enableTimeToIdle()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder()
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
 }
--- src/main/java/com/springlearning/CachingApplication/dto/EmployeeDto.java
+++ src/main/java/com/springlearning/CachingApplication/dto/EmployeeDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-public class EmployeeDto implements Serializable {
+public class EmployeeDto {
     private Long id;
     private String email;
     private String name;
